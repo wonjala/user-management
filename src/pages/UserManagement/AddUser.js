96f62b6e49018c02d59176acd0fbb87ac558eeed
Wonjala Joshi <wonju2joshi@gmail.com>
2024-03-18 07:43:53 +0545

    On Change Handle

--- src/pages/UserManagement/AddUser.js
+++ src/pages/UserManagement/AddUser.js
@@ -4,15 +4,31 @@ const AddUser = () => {
 
     const [username, setUsername] = useState();
     const [email, setEmail] = useState();
+    const [age, setAge] = useState();
+    const [city, setCity] = useState();
 
 
     const handleUserName = (e) => {
         setUsername(e.target.value);
     };
 
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleAge  = (e) => {
+        setAge(e.target.value);
+    };
+
+    const handleCity  = (e) => {
+        setCity(e.target.value);
+    };
     
     const saveForm = () => {
-        console.log("hellos");
+        console.log("username: ", username);
+        console.log("email: ", email);
+        console.log("age:", age);
+        console.log("city:", city);
 
     }
 
@@ -20,13 +36,21 @@ const AddUser = () => {
     return(
         <>
             <h1>Add user</h1>
-            <div>
+            <div className= "input-group">
                 <label>Username</label>
                 <input type="text" name="username" onChange={handleUserName} value={username}/>
             </div>
-            <div>
+            <div className= "input-group">
                 <label>Email</label>
-                <input type="text" name="email"/>
+                <input type="text" name="email" onChange={handleEmail} value={email}/>
+            </div>
+             <div className= "input-group">
+                <label>Age</label>
+                <input type="text" name="age" onChange={handleAge} value={age}/>
+            </div>
+            <div className= "input-group">
+                <label>City</label>
+                <input type="text" name="city" onChange={handleCity} value={city}/>
             </div>
 
             <div>
